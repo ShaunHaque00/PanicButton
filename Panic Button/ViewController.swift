249ef3e7d3fe94713a435f9fc3c375c705aadc83
mohammadhaque <mohammadhaque106@yahoo.com>
2015-11-27 22:53:53 -0500

    first commit

---
+++ Panic Button/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Panic Button
+//
+//  Created by Mohammad Haque on 11/11/15.
+//  Copyright © 2015 shaun Haque. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  
+    //declaration of ALL variables and outlets
+    
+    
+    @IBOutlet weak var PoliceButton: UIButton!
+    
+    @IBOutlet weak var ParamedicsButton: UIButton!
+    
+    @IBOutlet weak var FireDeptButton: UIButton!
+    
+    @IBOutlet weak var FamilyButton: UIButton!
+    
+    @IBOutlet weak var AlarmButton: UIButton!
+    
+    @IBOutlet weak var SettingsImage: UIImageView!
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
